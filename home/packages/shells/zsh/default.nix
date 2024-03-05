17fe5c93afefde8ff2f813fb2aaab7a0b5578ae0
kossLAN <kosslan@kosslan.dev>
2024-03-05 16:09:44 -0500

    feat: manage zsh exclusively with the hm module

--- home/packages/shells/zsh/default.nix
+++ home/packages/shells/zsh/default.nix
@@ -1,30 +1,75 @@
 { platform
 , lib
+, pkgs
 , ...
 }:
 let
   allowedPlatforms = [ "desktop" "macbook" ];
 in
 lib.mkIf (builtins.elem platform allowedPlatforms) {
-  programs.zsh = {
-    enable = true;
-    enableAutosuggestions = true;
-    enableCompletion = true;
-
-    oh-my-zsh = {
+  programs.zsh =
+    let
+      # Oh My Zsh has alot of good plugins, thankfully we can use them without needing oh-my-zsh!
+      oh-my-zsh = pkgs.fetchFromGitHub {
+        owner = "ohmyzsh";
+        repo = "ohmyzsh";
+        rev = "fd01fd66ce27c669e5ffaea94460a37423d1e134";
+        sha256 = "sha256-5G96Iae543/CVmwRVpwAlbRB7vf+t/E2fl0pOu+RM6Y=";
+      };
+    in
+    {
       enable = true;
-      custom = "$HOME/.config/zsh-custom";
-      plugins = [
-        "git"
-        "urltools"
-        "bgnotify"
-        "zsh-history-enquirer"
-        "jovial"
-      ];
-      # https://github.com/zthxxx/jovial <---- Good theme
-      theme = "jovial";
-    };
-  };
+      enableAutosuggestions = true;
+      enableCompletion = true;
 
-  home.file.".config/zsh-custom".source = ./custom;
+      plugins = [
+        {
+          file = "jovial.zsh-theme";
+          name = "jovial";
+          src = pkgs.fetchFromGitHub {
+            owner = "zthxxx";
+            repo = "jovial";
+            rev = "701ea89b6dd2b9859dab32bd083a16643b338b47";
+            sha256 = "sha256-VVGBG0ZoL25v+Ht1QbnZMc1TqTiJvr211OvyVwNc3bc=";
+          };
+        }
+
+        # These are all plugins required by jovial...
+        {
+          file = "plugins/git/git.plugin.zsh";
+          name = "git";
+          src = oh-my-zsh;
+        }
+        {
+          file = "plugins/urltools/urltools.plugin.zsh";
+          name = "urltools";
+          src = oh-my-zsh;
+        }
+        {
+          file = "plugins/bgnotify/bgnotify.plugin.zsh";
+          name = "bgnotify";
+          src = oh-my-zsh;
+        }
+        {
+          file = "zsh-history-enquirer.plugin.zsh";
+          name = "zsh-history-enquirer";
+          src = pkgs.fetchFromGitHub {
+            owner = "zthxxx";
+            repo = "zsh-history-enquirer";
+            rev = "6fdfedc4e581740e7db388b36b5e66f7c86e8046";
+            sha256 = "sha256-/RGBIoieqexK2r4onFbXAt4ALEIb17mn/all0P1xFkE=";
+          };
+        }
+        {
+          file = "zsh-syntax-highlighting.zsh";
+          name = "zsh-syntax-highlighting";
+          src = pkgs.fetchFromGitHub {
+            owner = "zsh-users";
+            repo = "zsh-syntax-highlighting";
+            rev = "e0165eaa730dd0fa321a6a6de74f092fe87630b0";
+            sha256 = "sha256-4rW2N+ankAH4sA6Sa5mr9IKsdAg7WTgrmyqJ2V1vygQ=";
+          };
+        }
+      ];
+    };
 }

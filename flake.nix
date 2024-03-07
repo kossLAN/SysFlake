e4411345a104cdbf191747d57a10368cc6e17acb
kossLAN <kosslan@kosslan.dev>
2024-03-07 01:48:46 -0500

    refactor: clean up flake/syncthing host

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Package Flakes
-    #TODO: Clean this shit up/format better
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -89,7 +88,7 @@
       };
 
       # LXC Images
-      syncthing.x86_64-linux = nixos-generators.nixosGenerate {
+      syncthing = nixos-generators.nixosGenerate {
         system = "x86_64-linux";
         modules = [
           ./hosts/syncthing
--- hosts/syncthing/default.nix
+++ hosts/syncthing/default.nix
@@ -56,10 +56,6 @@
       enable = true;
     };
 
-    nginx = {
-      enable = true;
-    };
-
     syncthing = {
       enable = true;
       guiAddress = "127.0.0.1:8384";

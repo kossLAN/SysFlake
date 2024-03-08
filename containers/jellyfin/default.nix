d082ca305e53eb1e5dca1f82e6bb6f90ea6468d3
kossLAN <kosslan@kosslan.dev>
2024-03-07 23:26:12 -0500

    feat: jellyfin folder

---
+++ containers/jellyfin/default.nix
@@ -0,0 +1,57 @@
+{ config
+, lib
+, pkgs
+, outputs
+, inputs
+, ...
+}: {
+  nixpkgs = {
+    hostPlatform = "x86_64-linux";
+    overlays = [
+      outputs.overlays.additions
+      outputs.overlays.modifications
+    ];
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+  nix = {
+    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+    optimise.automatic = true;
+    gc.automatic = true;
+    gc.options = "--delete-older-than 1d";
+
+    settings = {
+      experimental-features = "nix-command flakes";
+      auto-optimise-store = true;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+  ];
+
+  networking = {
+    hostName = "jellyfin";
+    domain = "";
+    firewall.allowedTCPPorts = [ 8096 8920 ];
+    firewall.allowedUDPPorts = [ 1900 7359 ];
+  };
+
+  services = {
+    openssh = {
+      enable = true;
+    };
+
+    jellyfin = {
+      enable = true;
+    };
+  };
+
+  users.users.root.openssh.authorizedKeys.keys = [ ''ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCpkeLOreGeqUDLcrlYgzyeSSZmBvJLY+dWOeORIpGQQVRvlko8NRcVKS/fa5EHBd9HG9gRs96FK5WF9JJCGsY4ovL++WZwlsQN3xfc0xq2Sn8TQhgDgiBFCR05JDMi1+f6v9WpaiLiQnOKiTmSGYhzvayIr/XrpcAaXo0mLDEnqZbSzqTcAcqZMcPZixmkgFJA+kUq6d1Z5XMPRRTPJNmLGY0jNbVlUiI9pWsIlGqZFcMLssNWnIZkl8SCV/lN+uyFy2G1o1LlMQ6UFziqP3Zm28gq6alt7ivFJ8A8hUffiZWeQ4uURV8TKhQ43FGSUspma7DpG5zGdionkN521rQJajdnWJLO25dXRkDdXWmkwpFuKRep0m0xv0VSxXAPYs5IrFuDuylbfo6W0N5dx2sPgBK8cQ2uj5AvVCM6g8cgWh+pxzG/WV/2XpwrT7jD8vyRUL+U6FpiMQIsepJ/WQIhA7HkQnex2QHGAsu7hP5Wr5Bs33m8JYT5XCT0KsXkzQE= koss@galahad'' ];
+
+  system.stateVersion = "23.11";
+}

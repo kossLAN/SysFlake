8cfc2b0bdd0e1d6ad8f8cf8495ad135942d70b90
kossLAN <kosslan@kosslan.dev>
2024-03-10 03:05:00 -0400

    feat: added smb to plex for ease of use

--- hosts/plex/default.nix
+++ hosts/plex/default.nix
@@ -53,6 +53,36 @@
     plex = {
       enable = true;
     };
+
+    samba = {
+      enable = true;
+      securityType = "user";
+      openFirewall = true;
+      extraConfig = ''
+        workgroup = WORKGROUP
+        server string = smbnix
+        netbios name = smbnix
+        security = user
+        #use sendfile = yes
+        #max protocol = smb2
+        hosts allow = 192.168.10.103 127.0.0.1 localhost
+        hosts deny = 0.0.0.0/0
+        guest account = nobody
+        map to guest = bad user
+      '';
+      shares = {
+        public = {
+          path = "/media";
+          browseable = "yes";
+          "read only" = "no";
+          "guest ok" = "yes";
+          "create mask" = "0644";
+          "directory mask" = "0755";
+          "force user" = "ncloud";
+          "force group" = "users";
+        };
+      };
+    };
   };
 
   users.users.root.initialPassword = "root";

296d7e43ea691168911f82768c784c2cb0bd51b7
kossLAN <kosslan@kosslan.dev>
2024-03-11 01:11:52 -0400

    fix: increase nginx body size

--- hosts/nginx/default.nix
+++ hosts/nginx/default.nix
@@ -77,7 +77,9 @@
             proxyWebsockets = true; # needed if you need to use WebSocket
             extraConfig =
               # required when the target is also TLS server with multiple hosts
-              "proxy_ssl_server_name on;"
+              "client_max_body_size 100M;"
+              #+ "client_body_buffer_size 400M;"
+              + "proxy_ssl_server_name on;"
               +
               # required when the server wants to use HTTP Authentication
               "proxy_pass_header Authorization;";

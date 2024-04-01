ffeeba0c846d7ab1e772ed1379eb67c28f6605ba
kossLAN <kosslan@kosslan.dev>
2024-04-01 10:25:10 -0400

    feat: continuation of modulization overhaul

--- modules/core/loginmanager/greetd/default.nix
+++ modules/core/loginmanager/greetd/default.nix
@@ -1,35 +1,3 @@
 {
-  pkgs,
-  lib,
-  config,
-  ...
-}: let
-  cfg = config.programs.gtkgreet;
-in {
-  options.programs.gtkgreet = {
-    enable = lib.mkEnableOption "gtkgreet";
-  };
-
-  config = let
-    hyprConfig = pkgs.writeText "greetd-hyprland-config" ''
-      misc {
-        disable_hyprland_logo = true;
-        disable_splash_rendering = true;
-      }
-      exec-once = ${pkgs.greetd.gtkgreet}/bin/gtkgreet -s ${./style.css} -c Hyprland; hyprctl dispatch exit
-    '';
-  in
-    lib.mkIf cfg.enable {
-      # I use hyprland for launching the login-manager, however I also have it installed
-      # via home-manager for regular use, so this is a weird situation of which I don't
-      # know what the proper solution is...
-      services.greetd = {
-        enable = true;
-        settings = {
-          default_session = {
-            command = "${pkgs.hyprland}/bin/Hyprland --config ${hyprConfig}";
-          };
-        };
-      };
-    };
+  imports = [ ./gtkgreet ];
 }

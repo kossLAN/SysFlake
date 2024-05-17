7a7cf6ebe7c69653216d28789b37400eda8ef1fe
kossLAN <kosslan@kosslan.dev>
2024-05-17 01:45:19 -0400

    fix: macos trampoline apps

--- modules/darwin/programs/hm/utils/trampoline/default.nix
+++ modules/darwin/programs/hm/utils/trampoline/default.nix
@@ -1,13 +1,12 @@
-{ lib
-, config
-, inputs
-, ...
-}:
-let
+{
+  lib,
+  config,
+  inputs,
+  ...
+}: let
   cfg = config.programs.hm.utils.trampoline;
   hmLib = inputs.home-manager.lib;
-in
-{
+in {
   imports = [
     inputs.home-manager.darwinModules.home-manager
   ];
@@ -20,14 +19,14 @@ in
   config = lib.mkIf cfg.enable {
     home-manager.users.${config.users.defaultUser} = {
       home.activation = {
-        trampolineApps =
-          let
-            mac-app-util = inputs.mac-app-util.packages.aarch64-darwin.default;
-          in
-          hmLib.hm.dag.entryAfter [ "writeBoundary" ] ''
-            fromDir="$HOME/Applications/Home Manager Apps"
-            toDir="$HOME/Applications/Home Manager Trampolines"
-            ${mac-app-util}/bin/mac-app-util sync-trampolines "$fromDir" "$toDir"
+        trampolineApps = let
+          mac-app-util = inputs.mac-app-util.packages.aarch64-darwin.default;
+          fromDir = "/Users/${config.users.defaultUser}/Applications/Home Manager Apps/";
+          toDir = "/Users/${config.users.defaultUser}/Applications/Home Manager Trampolines/";
+        in
+          hmLib.hm.dag.entryAfter ["writeBoundary"] ''
+            echo 'mac-app-util sync-trampolines ${fromDir} ${toDir}' > ~/macAppLog.txt
+            ${mac-app-util}/bin/mac-app-util sync-trampolines '${fromDir}' '${toDir}'
           '';
       };
     };
